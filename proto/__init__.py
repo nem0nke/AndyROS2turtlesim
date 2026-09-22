e4d9b15bbf511c6cd31496bfd8426faced13978c
Andy Wong <andy@Andys-MacBook-Air.local>
2026-09-22 02:45:21 -0400

    main initialization w/ protobuf setup

---
+++ proto/__init__.py
@@ -0,0 +1,4 @@
+"""Protocol Buffer schema and generated Python modules.
+
+TODO: Regenerate Python bindings here after changing ``turtle_command.proto``.
+"""
---
+++ src/__init__.py
@@ -0,0 +1,4 @@
+"""Application source package.
+
+TODO: Add the command-line and ROS 2 application modules here.
+"""
